ed274924ead0e1054651e6f19c46f6524176dd3d
Arek Ruta <arek.ruta89@gmail.com>
2017-11-03 19:21:27 +0100

    ChatService DialogFlow

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { ApiAiClient } from 'api-ai-javascript';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { environment } from '../../environments/environment';
+
+export class Message {
+  constructor(public content: string, public sentBy: string) {}
+}
 
 @Injectable()
 export class ChatService {
+  readonly token = environment.dialogflow.angularBot;
+  readonly client = new ApiAiClient({ accessToken: this.token });
 
-  constructor() { }
+  conversation = new BehaviorSubject<Message[]>([]);
 
+  constructor() {}
+
+  // Wysyłanie i otrzymywanie wiadomosci z DialogFlow
+  converse(msg: string) {
+    const userMessage = new Message(msg, 'user');
+    this.update(userMessage);
+
+    return this.client.textRequest(msg).then(res => {
+      const speech = res.result.fulfillment.speech;
+      const botMessage = new Message(speech, 'bot');
+      this.update(botMessage);
+    });
+  }
+
+  // Dodawanie wiadomosci do źrodla
+  update(msg: Message) {
+    this.conversation.next([msg]);
+  }
 }
