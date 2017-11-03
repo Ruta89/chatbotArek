a5c1442872c53fc6ef84833a9c92add796e09fe4
Arek Ruta <arek.ruta89@gmail.com>
2017-11-03 20:48:06 +0100

    ChatDialogComponent messages

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService, Message } from '../chat.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/scan';
 
 @Component({
   selector: 'app-chat-dialog',
   templateUrl: './chat-dialog.component.html',
-  styleUrls: ['./chat-dialog.component.css']
+  styleUrls: ['./chat-dialog.component.scss']
 })
 export class ChatDialogComponent implements OnInit {
-  constructor() {}
+  messages: Observable<Message[]>;
+  formValue: string;
 
-  ngOnInit() {}
+  constructor(public chat: ChatService) {}
+
+  ngOnInit() {
+    // dołącza do tablicy po każdej nowej wiadomości dodanej do feedSource (źrodlo zrodlowe)
+    this.messages = this.chat.conversation
+      .asObservable()
+      .scan((acc, val) => acc.concat(val));
+  }
+
+  sendMessage() {
+    this.chat.converse(this.formValue);
+    this.formValue = '';
+  }
 }
--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ChatDialogComponent } from './chat-dialog/chat-dialog.component';
 import { ChatService } from './chat.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   declarations: [ChatDialogComponent],
   exports: [ChatDialogComponent],
   providers: [ChatService]
